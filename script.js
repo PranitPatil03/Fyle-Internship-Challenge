bf282ebf17c8c29e84360d24fafa5ea76c7ebf56
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-04-11 16:34:16 +0530

    Challenge UI Completed ✅

--- script.js
+++ script.js
@@ -1,44 +1,11 @@
-// const annual_income = document.getElementById("annual_income");
-// const extra_income = document.getElementById("extra_income");
-// const deductions = document.getElementById("deductions");
-// const age = document.getElementById("age");
-// const submit = document.getElementById("submit");
-// const form = document.getElementById("form");
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const form = document.getElementById("form");
-
-//   form.addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-//     const annual_income_value = document.getElementById("annual_income").value;
-//     const extra_income_value = document.getElementById("extra_income").value;
-//     const deductions_value = document.getElementById("deductions").value;
-//     const age_value = document.querySelector(".age").value;
-
-//     console.log("annual_income_value", annual_income_value);
-//     console.log("extra_income_value", extra_income_value);
-//     console.log("deductions_value", deductions_value);
-//     console.log("age_value", age_value);
-//   });
-// });
-
-// const tooltipTriggerList = [].slice.call(
-//   document.querySelectorAll('[data-bs-toggle="tooltip"]')
-// );
-
-// const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//   return new bootstrap.Tooltip(tooltipTriggerEl);
-// });
-
-$(document).ready(function() {
+$(document).ready(function () {
   const $form = $("#form");
   const $annual_income = $("#annual_income");
   const $extra_income = $("#extra_income");
   const $deductions = $("#deductions");
   const $age = $("#age");
 
-  $form.submit(function(event) {
+  $form.submit(function (event) {
     event.preventDefault();
 
     const annual_income_value = $annual_income.val();
@@ -50,6 +17,23 @@ $(document).ready(function() {
     console.log("extra_income_value", extra_income_value);
     console.log("deductions_value", deductions_value);
     console.log("age_value", age_value);
+  });
+
+  function checkRequiredFields() {
+    var isValid = true;
+    $("#form :input[required]").each(function () {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+
+    $("#submit").prop("disabled", !isValid);
+  }
+
+  checkRequiredFields();
+
+  $("#form :input[required]").on("input", function () {
+    checkRequiredFields();
   });
 
   $('[data-bs-toggle="tooltip"]').tooltip();
